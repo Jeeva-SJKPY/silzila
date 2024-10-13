5f45e2256683ec3be4229b50b35f11645314b5a7
Manoj Balaji <balajihughes99rajpadma@gmail.com>
2024-10-13 08:07:06 +0530

    Add replace and split operations in text flow

--- src/main/java/com/silzila/querybuilder/calculatedField/selectClause/PostgresSelectClass.java
+++ src/main/java/com/silzila/querybuilder/calculatedField/selectClause/PostgresSelectClass.java
@@ -16,6 +16,34 @@ import com.silzila.querybuilder.calculatedField.ConditionFilterToFilter;
 
 public class PostgresSelectClass {
 
+    private final Map<String, String> basicMathOperations = Map.of(
+                "addition", "+",
+                "subtraction", "-",
+                "multiplication", "*",
+                "division", "/",
+                "ceiling","CEIL",
+                "floor","FLOOR",
+                "absolute","ABS",
+                "power","POWER",
+                "min", "LEAST",
+                "max","GREATEST"
+        );
+
+        private final static Map<String, String> basicTextOperations = Map.ofEntries(
+            Map.entry("concat", "CONCAT"),
+            Map.entry("propercase", "INITCAP"),
+            Map.entry("lowercase", "LOWER"),
+            Map.entry("uppercase", "UPPER"),
+            Map.entry("trim", "TRIM"),
+            Map.entry("ltrim", "LTRIM"),
+            Map.entry("rtrim", "RTRIM"),
+            Map.entry("length", "LENGTH"),
+            Map.entry("substringright", "RIGHT"),
+            Map.entry("substringleft", "LEFT"),
+            Map.entry("replace", "REPLACE"),
+            Map.entry("split", "SPLIT_PART")
+        );
+        
     public static String calculatedFieldsComposed(List<CalculatedFieldRequest> calculatedFieldRequests) {
         StringBuilder calculatedFieldString = new StringBuilder();
     
@@ -236,7 +264,7 @@ public class PostgresSelectClass {
                                             Map<String, String> basicMathOperations, List<String> result,
                                             String flowKey,List<String> source,List<String> sourceType) {
         for (int i = 0; i < source.size(); i++) {
-            String processedSource = getProcessedSource(source.get(i), sourceType.get(i), fields, flowStringMap, flow, i);
+            String processedSource = getMathProcessedSource(source.get(i), sourceType.get(i), fields, flowStringMap, flow, i);
             result.add(processedSource);
             if (i < source.size() - 1) {
                 result.add(basicMathOperations.get(flow.getFlow()));
@@ -249,7 +277,7 @@ public class PostgresSelectClass {
     private static void processMathSingleArgumentOperations(Flow flow, Map<String, Field> fields, Map<String, String> flowStringMap,
                                                         Map<String, String> basicMathOperations, String flowKey,
                                                         List<String> source, List<String> sourceType) {
-        String processedSource = getProcessedSource(source.get(0), sourceType.get(0), fields, flowStringMap, flow, 0);
+        String processedSource = getMathProcessedSource(source.get(0), sourceType.get(0), fields, flowStringMap, flow, 0);
         flowStringMap.put(flowKey, basicMathOperations.get(flow.getFlow()) + "(" + processedSource + ")");
     }
 
@@ -258,7 +286,7 @@ public class PostgresSelectClass {
                                                         Map<String, String> basicMathOperations, String flowKey,
                                                         List<String> result, List<String> source, List<String> sourceType) {
         for (int i = 0; i < source.size(); i++) {
-            String processedSource = getProcessedSource(source.get(i), sourceType.get(i), fields, flowStringMap, flow, i);
+            String processedSource = getMathProcessedSource(source.get(i), sourceType.get(i), fields, flowStringMap, flow, i);
             result.add(processedSource);
             if (i < source.size() - 1) {
                 result.add(",");
@@ -272,12 +300,12 @@ public class PostgresSelectClass {
     private static void processPowerOperation(Flow flow, Map<String, Field> fields, Map<String, String> flowStringMap,
                                             Map<String, String> basicMathOperations, String flowKey,
                                             List<String> source, List<String> sourceType) {
-        String processedSource = getProcessedSource(source.get(0), sourceType.get(0), fields, flowStringMap, flow, 0);
+        String processedSource = getMathProcessedSource(source.get(0), sourceType.get(0), fields, flowStringMap, flow, 0);
         flowStringMap.put(flowKey, basicMathOperations.get(flow.getFlow()) + "(" + processedSource + "," + source.get(1) + ")");
     }
 
     // to get a list of source with and without aggregation
-    private static String getProcessedSource(String source, String sourceType,
+    private static String getMathProcessedSource(String source, String sourceType,
                                              Map<String, Field> fields, Map<String, String> flowStringMap, Flow flow, int index) {
         String processedSource = "";
         if ("field".equals(sourceType)) {
@@ -299,52 +327,77 @@ public class PostgresSelectClass {
         String flowType = firstFlow.getFlow();
         StringBuilder result = new StringBuilder();
     
-        List<String> processedSources = processSources(firstFlow, fields, flowStringMap);
-
-        List<String> aggregation = firstFlow.getAggregation();
-        String length = aggregation != null && !aggregation.isEmpty() ? aggregation.get(0) : "0";
+        List<String> processedSources = processTextSources(firstFlow, fields, flowStringMap);
     
-        switch (flowType) {
-            case "concat":
-                result.append("CONCAT (\n")
-                      .append(String.join(" ,\n", processedSources))
-                      .append(")");
-                break;
-    
-            case "uppercase":
-                result.append("UPPER (")
-                      .append(String.join("", processedSources))
-                      .append(")");
-                break;
-    
-            case "lowercase":
-                result.append("LOWER (")
-                      .append(String.join("", processedSources))
-                      .append(")");
-                break;
-            case "substringright":
-                result.append("RIGHT(")
-                    .append(processedSources.get(0))
-                    .append(", ")
-                    .append(length)
-                    .append(")");
-                break;
-            case "substringleft":
-                result.append("LEFT(")
-                    .append(processedSources.get(0))
-                    .append(", ")
-                    .append(length)
-                    .append(")");
-                break;
-            
+        if(List.of("concat","propercase","uppercase","lowercase","trim","ltrim","rtrim","length").contains(flowType)){
+            result.append(processTextSingleArgumentOperations(flowType,processedSources));
+        }
+        else if(List.of("substringright","substringleft").contains(flowType)){
+            result.append(processSubStringOperations(firstFlow,flowType,processedSources));
+        }
+        else if ("replace".equals(flowType)){
+            result.append(processTextReplaceOperation(firstFlow,flowType,processedSources));
+        }
+        else if ("split".equals(flowType)){
+            result.append(processTextSplitOperation(firstFlow,flowType,processedSources));
         }
     
         // Store the result in flowStringMap using the flowKey
         flowStringMap.put(flowKey, result.toString());
         return result.toString();
     }
+
+    // to process text operation - propercase,lowercase....
+    private static String processTextSingleArgumentOperations(String flowType,List<String> processedSources){
+
+            StringBuilder result = new StringBuilder();
+            result.append(basicTextOperations.get(flowType)).append(" (")
+                      .append(String.join(", ", processedSources))
+                      .append(")");
+            return result.toString();
+    }
+
+    // to process substring operation
+    //1st source - String to extract from, 2nd source - number of chars to extract 
+    private static String processSubStringOperations(Flow flow,String flowType,List<String> processedSources){
+        StringBuilder result = new StringBuilder();
+        result.append(basicTextOperations.get(flowType)).append(" (")
+                    .append(processedSources.get(0))
+                    .append(", ")
+                    .append(flow.getSource().get(1))
+                    .append(")");
+        return result.toString();
+    }
+
+    // to process text replace operation
+    //1st source - string, 2nd source - substring to be replaced, 3rd source - replacement to the replaced substring
+    private static String processTextReplaceOperation(Flow flow,String flowType,List<String> processedSources){
+        StringBuilder result = new StringBuilder();
+        result.append(basicTextOperations.get(flowType)).append(" (")
+                    .append(processedSources.get(0))
+                    .append(", '")
+                    .append(flow.getSource().get(1))
+                    .append("', '")
+                    .append(flow.getSource().get(2))
+                    .append("')");
+        return result.toString();
+    }
+
+    // to process text split operation
+    //1st source - string, 2nd source - delimiter, 3rd source - position(substring to be returned)
+    private static String processTextSplitOperation(Flow flow,String flowType,List<String> processedSources){
+        StringBuilder result = new StringBuilder();
+        result.append(basicTextOperations.get(flowType)).append(" (")
+                    .append(processedSources.get(0))
+                    .append(", '")
+                    .append(flow.getSource().get(1))
+                    .append("',")
+                    .append(flow.getSource().get(2))
+                    .append(")");
+        return result.toString();
+    }
     
-    private static List<String> processSources(Flow firstFlow, Map<String, Field> fields, Map<String, String> flowStringMap) {
+    private static List<String> processTextSources(Flow firstFlow, Map<String, Field> fields, Map<String, String> flowStringMap) {
         List<String> source = firstFlow.getSource();
         List<String> sourceType = firstFlow.getSourceType();
         List<String> resultString = new ArrayList<>();
@@ -359,7 +412,8 @@ public class PostgresSelectClass {
             } else if ("flow".equals(type)) {
                 resultString.add(flowStringMap.get(sourceElement));
             } else {
-                resultString.add(sourceElement);
+
+                resultString.add("'"+sourceElement+"'");
             }
         }
     

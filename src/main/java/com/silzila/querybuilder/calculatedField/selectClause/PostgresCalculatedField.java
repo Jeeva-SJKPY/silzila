9dcff91b2694f727261291413e0da545637a8e16
Jeeva-SJKPY <sjeeva.mail@gmail.com>
2024-10-21 16:28:30 +0530

    changes in Mysql string

--- src/main/java/com/silzila/querybuilder/calculatedField/selectClause/PostgresCalculatedField.java
+++ src/main/java/com/silzila/querybuilder/calculatedField/selectClause/PostgresCalculatedField.java
@@ -456,12 +456,10 @@ public class PostgresCalculatedField {
     
         if ("if".equals(firstFlow.getCondition())) {
             String result = conditionFlowDateType(flows, fields, firstFlow);
-            if (result != null) {
-                return result;
-            }
+            return  result;
         }
     
-        if (basicMathOperations.containsKey(flowType)
+        else if (basicMathOperations.containsKey(flowType)
             || List.of("dateInterval", "datePartNumber").contains(flowType)
             || firstFlow.getIsAggregation()) {
             return "integer";
@@ -490,8 +488,10 @@ public class PostgresCalculatedField {
             }
             return "unknown";
         }
-    
-        return null;
+
+        else{
+            return sourceType;
+        }
     }
     
 }
